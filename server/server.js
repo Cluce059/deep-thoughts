b8f753bf0d66515f1c46c816bd2416447112bfb6
caroline <cluce059@gmail.com>
2021-10-29 21:25:28 -0400

    initil queries

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const startServer = async () => {
+  //create apollo server to take data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    //context: authMiddleware,
+
+  });
+
+    //start server
+    await server.start();
+
+    //integrate apollo server w express app ad middleware
+    server.applyMiddleware({ app });
+
+    console.log(`use graphQL at http://localhost:${PORT}${server.graphqlPath}`)
+};
+
+//init apollo server
+startServer();
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
